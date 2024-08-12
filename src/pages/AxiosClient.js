33fb37ec5c9eb2ce9b13de660c618b38d6e00244
Joweb1 <nahjonah00@gmail.com>
2024-08-12 11:29:23 -0600

    Final Update 11

--- src/pages/AxiosClient.js
+++ src/pages/AxiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: import.meta.env.VITE_API_BASE_URL
 })
 
 axiosClient.interceptors.request.use((config) => {
